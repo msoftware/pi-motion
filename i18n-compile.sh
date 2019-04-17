b60cd4385e4fb83ae1345ec07af74c4c51c33d63
Michael Jentsch <M.Jentsch@web.de>
2019-04-17 13:46:10 +0200

    Update i18n gen. scripts

--- i18n-compile.sh
+++ i18n-compile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+DIR=`pwd`
 mv sun.py sun.bak
-python -m i18n --root=/home/pi/pi-motion --languages=en_US,de_DE compile
+python -m i18n --root=$DIR --languages=en_US,de_DE compile
 mv sun.bak sun.py
 
--- i18n-extract.sh
+++ i18n-extract.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
-mv sun.py sun.bak
-python -m i18n --root=/home/pi/pi-motion --languages=en_US,de_DE extract
-mv sun.bak sun.py
+DIR=`pwd`
+
+while true; do
+    echo
+    echo "If you continue all existing text files will be deleted!"
+    echo
+    read -p "Do you wish to extract texts? (y/n)" yn
+    case $yn in
+        [Yy]* ) 
+            mv sun.py sun.bak
+            python -m i18n --root=$DIR --languages=en_US,de_DE extract
+            mv sun.bak sun.py
+            break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+exit
+
 
